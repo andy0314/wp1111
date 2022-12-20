3052bc87eca931ea8d292970a4eabb96b1bc4c0b
張佑亘 <b10902021@ntu.edu.tw>
2022-12-20 11:18:17 +0800

    hack3

--- hack3/frontend/src/components/Home.js
+++ hack3/frontend/src/components/Home.js
@@ -63,7 +63,22 @@ function Home() {
   );
   
   // TODO 6.5 Logic of subscription
-  
+  useEffect(
+    () => {
+      subscribeToMore({
+        document: ITEM_DELETED_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev;
+          const deletedItem = subscriptionData.data.itemDeleted;
+          console.log(deletedItem)
+          return {
+            items: prev.items.filter((x) => x.id !== deletedItem)
+          };
+        },
+      });
+    },
+    [subscribeToMore],
+  );
   // TODO 6.5 End 
 
   if (loading) return <p>Loading...</p>;
